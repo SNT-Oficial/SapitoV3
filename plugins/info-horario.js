cd5e8652651b2d96159e785f6233ef9503ae8c72
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-19 16:24:19 -0300

    Update info-horario.js

--- plugins/info-horario.js
+++ plugins/info-horario.js
@@ -23,25 +23,25 @@ const fechabra = moment().tz('America/Sao_Paulo').format('DD/MM HH:mm')
 const fechaafri = moment().tz('Africa/Malabo').format('DD/MM HH:mm')
 await conn.sendMessage(m.chat, { text: `\`\`\`
 「 ZONA-HORARIA ⏰ 」
-⏱️Peru       : ${fechaper}
-⏱️Mexico     : ${fechamex}
-⏱️Bolivia    : ${fechabol}
-⏱️Chile      : ${fechachi}
-⏱️Argentina  : ${fechaarg}
-⏱️Colombia   : ${fechacol}
-⏱️Ecuador    : ${fechaecu}
-⏱️Costa_Rica : ${fechacosr}
-⏱️Cuba       : ${fechacub}
-⏱️Guatemala  : ${fechagua}
-⏱️Honduras   : ${fechahon}
-⏱️Nicaragua  : ${fechanic}
-⏱️Panama     : ${fechapan}
-⏱️Uruguay    : ${fechauru}
-⏱️Venezuela  : ${fechaven}
-⏱️Paraguay   : ${fechapar}
-⏱️New York   : ${fechanew}
+⏱️Mexico 🇲🇽    : ${fechamex}
+⏱️Paraguay 🇵🇾  : ${fechapar}
+⏱️Peru 🐒      : ${fechaper}
+⏱️Bolivia 🇧🇴   : ${fechabol}
+⏱️Chile 🇨🇱     : ${fechachi}
+⏱️Argentina 🇦🇷 : ${fechaarg}
+⏱️Colombia 🇨🇴  : ${fechacol}
+⏱️Ecuador 🇪🇨   : ${fechaecu}
+⏱️Costa_Rica 🇨🇷: ${fechacosr}
+⏱️Cuba 🇨🇺      : ${fechacub}
+⏱️Guatemala 🇬🇹  : ${fechagua}
+⏱️Honduras  🇭🇳 : ${fechahon}
+⏱️Nicaragua 🇳🇮  : ${fechanic}
+⏱️Panama 🇵🇦    : ${fechapan}
+⏱️Uruguay  🇺🇾  : ${fechauru}
+⏱️Venezuela 🇻🇪 : ${fechaven}
+⏱️New York 🇺🇲  : ${fechanew}
 ⏱️Asia       : ${fechaasi}
-⏱️Brasil     : ${fechabra}
+⏱️Brasil 🇧🇷     : ${fechabra}
 ⏱️G.N.Q      : ${fechaafri}
 \`\`\`
 ${String.fromCharCode(8206).repeat(850)}
